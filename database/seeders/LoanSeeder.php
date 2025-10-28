00bced772eacb213d524a5543735ab4f5184d8fc
hijjatulmuqarramah <230212009@student.ar-raniry.ac.id>
2025-10-28 16:21:34 +0700

    Tugas UTS

--- database/seeders/LoanSeeder.php
+++ database/seeders/LoanSeeder.php
@@ -1,17 +1,31 @@
 <?php
 
-namespace Database\Seeders;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-
-class LoanSeeder extends Seeder
+return new class extends Migration
 {
     /**
-     * Run the database seeds.
+     * Run the migrations.
      */
-    public function run(): void
+    public function up(): void
     {
-        //
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('member_id')->constrained()->onDelete('cascade');
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->date('loan_date');
+            $table->date('return_date')->nullable();
+            $table->timestamps();
+        });
     }
-}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
